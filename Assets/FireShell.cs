f30cad83b76faaeb15a536144df9acfa46ec7a86
Annanatal <anaaaaaacastros@gmail.com>
2026-09-18 19:18:08 +0100

    velocity...bue programção num dia

---
+++ Assets/FireShell.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+      public GameObject bullet;
+      public GameObject turret;
+      public GameObject enemy;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void CreateBullet()
+    {
+        Instantiate(bullet, turret.transform.position, turret.transform.rotation);
+    }
+
+    Vector3 CalculateTrajectory()
+    {
+        Vector3 p = enemy.transform.position - this.transform.position; 
+        Vector3 v = enemy.transform.forward * enemy.GetComponent<Drive>().speed;
+        float s = bullet.GetComponent<MoveShell>().speed;
+
+        float a = Vector3.Dot(v, v) - s * s;
+        float b = Vector3.Dot(p, v);
+        float c = Vector3.Dot(p, p);
+        float d = b * b - a * c;
+        if (d < 0.1f) return Vector3.zero;
+
+        float sqrt = Mathf.Sqrt(d);
+        float t1 = (-b - sqrt) / c;
+        float t2 = (-b + sqrt) / c;
+
+        float t = 0;
+        if(t1 < 0 && t2 < 0) t = 0;
+        else if(t1 < 0) t = t2;
+        else if(t2 < 0) t = t1;
+        else
+        {
+            t = Mathf.Max(new float[] { t1, t2});
+        }
+        return t * p + v;
+    } 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.transform.forward = CalculateTrajectory();
+            CreateBullet();
+        }
+}
+}
+
